8c30b874ae715f501ce8a442aee1465bddb61424
brayan-brs <84359269+brayan-brs@users.noreply.github.com>
2023-04-19 18:42:54 -0300

    Update User.java

--- userdept/src/main/java/com/devsuperior/userdept/entities/User.java
+++ userdept/src/main/java/com/devsuperior/userdept/entities/User.java
@@ -1,59 +1,28 @@
-package com.devsuperior.userdept.entities;
+package com.mycompany.myproject.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "tb_user")
 public class User {
+    private String username;
+    private String password;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-	private String email;
-	
-	@ManyToOne
-	@JoinColumn(name = "department_id")
-	private Department department;
-	
-	public User() {
-	}
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Department getDepartment() {
-		return department;
-	}
-
-	public void setDepartment(Department department) {
-		this.department = department;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
