7a3bf8f7da021b369750d54c967f0e8c3a02f117
brayan-brs <84359269+brayan-brs@users.noreply.github.com>
2023-04-20 15:49:36 -0300

    Create UserController.java

---
+++ userdept/src/main/openred/UserController.java
@@ -0,0 +1,33 @@
+
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password, Model model) {
+        if ("Usuario".equals(username) && "senha".equals(password)) {
+            return "redirect:/home";
+        } else {
+            model.addAttribute("error", "Usuário ou senha incorretos");
+            return "login";
+        }
+    }
+
+    @GetMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("error", "Usuário ou senha incorretos");
+        return "login";
+    }
+}
